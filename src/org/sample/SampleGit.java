991ab2abd21584f6279a321a8875e0ed557509c7
chithrak17699 <chithrak17699@gmail.com>
2022-03-27 11:39:03 +0530

    Build is ready to store in Master

---
+++ src/org/sample/SampleGit.java
@@ -0,0 +1,10 @@
+package org.sample;
+
+public class SampleGit {
+	public static void main(String[] args) {
+		System.out.println("Hii My Dear.....");
+		System.out.println("I am Chithu...");
+	}
+
+
+}
