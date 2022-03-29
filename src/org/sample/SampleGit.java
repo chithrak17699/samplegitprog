03063a0bbafea3b0f41d551f3ec42ed97c0c62ed
chithrak17699 <chithrak17699@gmail.com>
2022-03-29 21:13:07 +0530

    selenium done with code

--- src/org/sample/SampleGit.java
+++ src/org/sample/SampleGit.java
@@ -4,6 +4,8 @@ public class SampleGit {
 	public static void main(String[] args) {
 		System.out.println("Hii My Dear.....");
 		System.out.println("I am Chithu...");
+		System.out.println("I Love You Baby");
+		System.out.println("I Miss You Baby");
 	}
 
 
